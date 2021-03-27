161644812f7bef053695bff88ad1bbdef35fd55f
Robrecht Cannoodt <rcannood@gmail.com>
2021-03-27 21:24:00 +0100

    follow reticulate advice on how to manage reticulate dependencies

---
+++ R/zzz.R
@@ -0,0 +1,4 @@
+# following advice from https://rstudio.github.io/reticulate/articles/python_dependencies.html#onload-configuration
+.onLoad <- function(libname, pkgname) {
+  reticulate::configure_environment(pkgname)
+}
