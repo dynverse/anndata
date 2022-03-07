f974901003c3fbaeff4a38faff84f4185ff04557
Robrecht Cannoodt <rcannood@gmail.com>
2022-03-07 17:01:06 +0100

    add dimnames setter for AnnDataR6

--- R/class_anndata.R
+++ R/class_anndata.R
@@ -956,6 +956,21 @@ dimnames.AnnDataR6 <- function(x) {
 
 #' @rdname AnnDataHelpers
 #' @export
+`dimnames<-.AnnDataR6` <- function(x, value) {
+  d <- dim(x)
+  if (!is.list(value) || length(value) != 2L)
+    stop("invalid 'dimnames' given for AnnData")
+  # value[[1L]] <- as.character(value[[1L]])
+  # value[[2L]] <- as.character(value[[2L]])
+  if (d[[1L]] != length(value[[1L]]) || d[[2L]] != length(value[[2L]]))
+    stop("invalid 'dimnames' given for AnnData")
+  x$obs_names <- value[[1L]]
+  x$var_names <- value[[2L]]
+  x
+}
+
+#' @rdname AnnDataHelpers
+#' @export
 dim.AnnDataR6 <- function(x) {
   x$shape
 }
